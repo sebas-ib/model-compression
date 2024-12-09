aa47e48b7c084a191fa59a3c89d4046f7df847b1
sebas-ib <ibarraperezsebastian@gmail.com>
2024-12-09 14:15:30 -0800

    Inital Commit

---
+++ src/data_processing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json
+from sklearn.model_selection import train_test_split
+
+
+def csv_data_split(csv_path, test_size=0.20, random_state=42):
+    df = pd.read_csv(csv_path)
+    df = df.apply(pd.to_numeric, errors='coerce')
+
+    train_data, test_data = train_test_split(df, test_size=test_size, random_state=random_state)
+    sample_df = train_data.sample(n=int(len(train_data) / 2), random_state=random_state)
+    return train_data, test_data, sample_df
+
+
+def metadata(file_path):
+    with open(file_path) as f:
+        my_metadata_dict = json.load(f)
+    return my_metadata_dict
